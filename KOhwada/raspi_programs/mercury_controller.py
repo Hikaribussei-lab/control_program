d51cc16b82722b0c641195a3e06a6570c7aa8a53
Hikaribussei-lab <hikaribussei.ml@gmail.com>
2022-02-21 20:55:33 +0900

    クライアントとの連携に成功

--- KOhwada/raspi_programs/mercury_controller.py
+++ KOhwada/raspi_programs/mercury_controller.py
@@ -4,22 +4,11 @@ import random
 
 class MercuryController:
     """
-    テスト用のクラス
-    sin関数から呼び出しのたびに一つ値を出力する。
+    Mercuryをコントロールするための実行クラス。
     """
-
-    # def __init__(self):
-    #     st = np.linspace(0, 2*np.pi, 100)
-    #     self.sin = np.sin(self.t)
     
-    def get_data_from_mercury(self):
-        pass
-    
-    def randomtest(self):
+    def get_temperature(self):
         return f"{round(random.random(), 2)}"
     
-    def constanttest(self):
-        return "123"
-    
-    def timetest(self):
-        return datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+    def get_power(self):
+        return f"{round(random.random(), 2)}"
