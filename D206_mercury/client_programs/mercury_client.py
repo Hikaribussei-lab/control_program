b088b7e8440939a4a3b5f2957af2eb5132526a32
Hikaribussei <hikaribussei.ml@gmail.com>
2022-02-22 14:52:58 +0900

    move production files to D206_mercury

---
+++ D206_mercury/client_programs/mercury_client.py
@@ -0,0 +1,46 @@
+import socket
+from datetime import datetime
+import random
+
+class MercuryClient:
+
+    def __init__(self):
+        self.ip = "192.168.11.71"  # server IP address
+        self.portnum = 1025
+        
+        self.buffer_size = 1024  # 受信するコマンドの最大バイト数(２のべき乗の値にする)
+
+    def client_main(self, order):
+        """
+        サーバ(Raspberry pi)に命令を投げ、返り値を受け取る。
+        返り値例
+        DATE:20220221;TIME:16-31-52,TEMP:302.1;POW:120.3
+        """
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
+            soc.connect((self.ip, self.portnum))  # connect with server
+            
+            soc.sendall(order.encode())  # send order to server
+
+            data = soc.recv(self.buffer_size).decode()  # get the data from server as string
+        
+        return data
+
+    def get_data_from_mercury(self, order):
+        data_string = self.client_main(order)  # ex) DATE:20220221,TIME:16-31-52,TEMP:302.1,POW:120.3
+        data_dict = {}
+        for content in data_string.split(","):
+            _kind = content.split(":")[0]
+            _value = content.split(":")[1]
+            data_dict[_kind] = _value
+
+        return data_dict
+
+    def send_stop(self):
+        return self.client_main(order="stop")
+        
+
+
+
+
+
---
+++ D206_mercury/raspi_programs/mercury_server.py
@@ -0,0 +1,80 @@
+from datetime import datetime
+import socket
+import time
+
+import mercury_controller
+
+class MercuryServer:
+    """
+    
+    """
+    
+    def __init__(self):
+        self.portnum = 1025
+        self.ip_wired = "192.168.11.71"  #serverIP
+        
+        self.buffer_size = 1024  # 受信するコマンドの最大バイト数(２のべき乗の値にする)
+        
+        self.mc = mercury_controller.MercuryController()
+    
+    def server_setup(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind((self.ip_wired, self.portnum))
+            s.listen(10)
+            
+            quitFlag = 0
+            print("Waiting for connection... ")
+            while not quitFlag:
+                connection, address = s.accept()
+                with connection:
+                    quit_flag = False
+                    while not quit_flag:
+                        client_command = connection.recv(self.buffer_size).decode()  # command from client PC.
+                        if not client_command:
+                            break
+                        if client_command == "quit":
+                            quit_flag = 1
+                        elif client_command == "stop":
+                            self.mc.device.close()
+                            self.mc.rm.close()
+                            retrun_string = "Connection with Mercury is closed."
+                            connection.sendall(s.encode())
+                            print(retrun_string)
+                        else:
+                            data = self._server_operations(client_command)
+                            connection.sendall(data.encode())
+                            print(f"GET => {data}")
+                            print("Waiting for client's order...")
+                        
+    
+    def _server_operations(self, order):
+        """
+        クライアントPCからの命令に沿ったデータを取得し、文字列として返す。
+        """
+        orders = order.split(";")
+        
+        now = datetime.now()
+        snow = now.strftime('%Y/%m/%d %H-%M-%S')
+        sdate = snow.split(" ")[0]
+        stime = snow.split(" ")[1]
+        
+        get_time = time.time()  # UNIX時間
+        
+        datas = [f"DATE:{sdate}", f"TIME:{stime}", f"GETTIME:{get_time}"]
+        
+        if "TEMP" in orders:
+            _temp = self.mc.get_temperature()
+            datas.append(f"TEMP:{_temp}")
+        
+        if "POW" in orders:
+            _pow = self.mc.get_power()
+            datas.append(f"POW:{_pow}")
+        
+        data_string = ",".join(datas)
+        
+        return data_string
+
+if __name__ == "__main__":
+    ms = MercuryServer()
+    ms.server_setup()
